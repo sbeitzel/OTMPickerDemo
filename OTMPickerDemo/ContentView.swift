9b2407a704e60ab531854406d4cbea53d654a7a9
Stephen Beitzel <sbeitzel@pobox.com>
2022-06-03 08:59:33 -0700

    Initial Commit

---
+++ OTMPickerDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OTMPickerDemo
+//
+//  Created by Stephen Beitzel on 6/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OTMPickerDemo/OTMPickerDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  OTMPickerDemoApp.swift
+//  OTMPickerDemo
+//
+//  Created by Stephen Beitzel on 6/3/22.
+//
+
+import SwiftUI
+
+@main
+struct OTMPickerDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
